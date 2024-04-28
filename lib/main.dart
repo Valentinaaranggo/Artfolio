59c7e365d54be2b79bee28660e5a9e380b9ceca6
valentinaaranggo <valea28@gmail.com>
2024-04-27 22:51:14 -0400

    0.2.0

--- lib/main.dart
+++ lib/main.dart
@@ -235,3 +235,104 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 }
+
+class RegisterEmailSection extends StatefulWidget {
+  RegisterEmailSection({Key? key, required this.auth}) : super(key: key);
+  final FirebaseAuth auth;
+
+  @override
+  _RegisterEmailSectionState createState() => _RegisterEmailSectionState();
+}
+
+class _RegisterEmailSectionState extends State<RegisterEmailSection> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _success = false;
+  bool _initialState = true;
+  String? _userEmail;
+
+  void _register() async {
+    try {
+      await widget.auth.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(_emailController.text)
+          .set({
+        'email': _emailController.text,
+        'password': _passwordController.text,
+      });
+
+      setState(() {
+        _success = true;
+        _userEmail = _emailController.text;
+        _initialState = false;
+      });
+    } catch (e) {
+      setState(() {
+        _success = false;
+        _initialState = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: Text('Are you new here? Register Today!'),
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+          ),
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(labelText: 'Email'),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(labelText: 'Password'),
+            validator: (value) {
+              if (value?.isEmpty ?? true) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            alignment: Alignment.center,
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _register();
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              _initialState
+                  ? ''
+                  : _success
+                      ? 'Successfully registered $_userEmail'
+                      : 'Registration failed',
+              style: TextStyle(color: _success ? Colors.green : Colors.red),
+            ),
+          ),
+        ],
